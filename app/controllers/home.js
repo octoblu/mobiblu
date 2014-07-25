4332951da7c34292840e7bdf3f50d221b6f34e78
Peter DeMartini <peterdemartini@me.com>
2014-07-24 18:41:21 -0700

    Unfinished login

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,16 +1,19 @@
 'use strict';
 
 angular.module('main.home')
-    .controller('HomeCtrl', function ($rootScope, $scope, $location) {
+    .controller('HomeCtrl', function ($rootScope, $scope, $location, Auth) {
 
         $scope.init = function () {
-            $scope.login = $rootScope.Skynet.login;
             $rootScope.ready(function () {
                 $rootScope.loading = false;
             });
             $rootScope.pluginReady(function () {
                 $scope.subdevices = window.octobluMobile.getSubdevices();
             });
+        };
+
+        $scope.login = function(){
+            Auth.login();
         };
 
         $scope.goToDevice = function (subdevice) {
--- www/public/app/config.js
+++ www/public/app/config.js
@@ -1,8 +1,28 @@
 'use strict';
 
 //Setting HTML5 Location Mode
-angular.module('main').config(['$locationProvider',
-    function ($locationProvider) {
-        $locationProvider.hashPrefix('!');
-    }
-]);
+angular.module('main')
+    .config(['$locationProvider', '$httpProvider',
+        function ($locationProvider, $httpProvider) {
+
+            $locationProvider.hashPrefix('!');
+
+            $httpProvider.interceptors.push(function ($window) {
+                return {
+                    responseError: function (response) {
+                        if (response.status === 401) {
+                            $window.location = 'index.html#!/login';
+                            return;
+                        }
+                        if (response.status === 403) {
+                            $window.location = 'index.html#!/accept_terms';
+                            return;
+                        }
+                        return response;
+                    }
+                };
+
+            });
+
+        }
+    ]);
