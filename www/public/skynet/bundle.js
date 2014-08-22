95b59826b79f90a09f2e850357785a994e06eb49
Peter DeMartini <peterdemartini@me.com>
2014-08-22 11:01:01 -0700

    0.12.16: Better Push

--- www/public/skynet/bundle.js
+++ www/public/skynet/bundle.js
@@ -87,6 +87,7 @@ var SkynetRest = _dereq_('./skynet.js');
 var Topics = _dereq_('./topics.js');
 var Labels = _dereq_('./labels.js');
 var activity = _dereq_('./activity.js');
+var push = _dereq_('./push.js');
 
 var Q = Promise;
 
@@ -221,175 +222,7 @@ app.isRegistered = function() {
 };
 
 app.registerPushID = function() {
-    var deferred = defer();
-
-    var listening = false;
-
-    function isEnabled() {
-        return new Promise(function(resolve, reject) {
-            window.PushNotification
-                .isPushEnabled(function(status) {
-                    console.log('Push Status: ' + JSON.stringify(status));
-                    if (status || status === 'OK') {
-                        resolve();
-                    } else {
-                        reject();
-                    }
-                });
-        });
-    }
-
-    function registerPushID(pushID) {
-        app.pushID = pushID;
-        window.localStorage.setItem('pushID', app.pushID);
-
-        return new Promise(function(resolve, reject) {
-            app.updateDeviceSetting({})
-                .then(function() {
-                    activity.logActivity({
-                        type: 'push',
-                        html: 'Push ID Registered'
-                    });
-                    resolve();
-                }, function() {
-                    var msg = 'Push ID Updated Failed';
-                    activity.logActivity({
-                        type: 'push',
-                        error: new Error(msg)
-                    });
-                    reject();
-                });
-        });
-    }
-
-    function getPushID() {
-        return new Promise(function(resolve, reject) {
-            if (app.pushID) return resolve();
-            window.PushNotification
-                .getPushID(function(pushID) {
-                    console.log('Push ID: ' + pushID);
-
-                    activity.logActivity({
-                        type: 'push',
-                        html: 'Push ID: ' + JSON.stringify(pushID)
-                    });
-
-                    if (pushID) {
-                        resolve(pushID);
-                    } else {
-                        reject();
-                    }
-                });
-        });
-    }
-
-    function enable() {
-        return new Promise(function(resolve, reject) {
-            window.PushNotification
-                .enablePush(resolve);
-        });
-    }
-
-    function listen() {
-        if (listening) return console.log('Starting Push Notifications Logic');
-
-        console.log('Listening for Push Notifications');
-        listening = true;
-        document.addEventListener('urbanairship.push', handleIncomingPush, false);
-    }
-
-    function start() {
-        if (listening) return console.log('Starting Push Notifications Logic');
-
-        if (!window.PushNotification) return console.log('No Push Notification Object');
-
-        console.log('Starting Push Flow');
-
-        var a = window.PushNotification.notificationType.badge,
-            b = window.PushNotification.notificationType.sound,
-            c = window.PushNotification.notificationType.alert;
-
-        window.PushNotification.registerForNotificationTypes(a | b | c);
-
-        isEnabled()
-            .then(getPushID, function() {
-                return enable()
-                    .then(getPushID, function() {
-                        activity.logActivity({
-                            type: 'push',
-                            error: 'Unable to get Push ID'
-                        });
-                    })
-                    .then(registerPushID, function() {
-                        activity.logActivity({
-                            type: 'push',
-                            error: 'Unable to register Push ID'
-                        });
-                    })
-                    .done(listen);
-            })
-            .done(listen);
-    }
-
-    function onRegistration(event) {
-        if (event.error) {
-            // Not Registered
-            var msg = 'Urbanairship Registration Error';
-
-            activity.logActivity({
-                type: 'push',
-                error: event.error
-            });
-
-            deferred.reject(msg);
-
-        } else {
-
-            activity.logActivity({
-                type: 'push',
-                html: 'Push ID Registered: ' + event.pushID
-            });
-
-            // Registered
-            app.pushID = event.pushID;
-            window.localStorage.setItem('pushID', app.pushID);
-
-            // Start Listen
-            start();
-        }
-    }
-
-    function handleIncomingPush(event) {
-        console.log('Incoming push: ' + event.message);
-
-        activity.logActivity({
-            type: 'push',
-            html: 'Received Push Notification: ' + event.message
-        });
-
-    }
-
-    document.addEventListener('resume', function() {
-        console.log('Push: Device resume!');
-
-        PushNotification.resetBadge();
-        PushNotification.getIncoming(handleIncomingPush);
-
-        // Reregister for urbanairship events if they were removed in pause event
-        document.addEventListener('urbanairship.registration', onRegistration, false);
-        document.addEventListener('urbanairship.push', handleIncomingPush, false);
-    }, false)
-
-    document.addEventListener('pause', function() {
-        console.log('Push: Device pause!');
-        // Remove urbanairship events.  Important on android to not receive push in the background.
-        document.removeEventListener('urbanairship.registration', onRegistration, false);
-        document.removeEventListener('urbanairship.push', handleIncomingPush, false);
-    }, false);
-
-    start();
-
-    return deferred.promise;
+    return push();
 };
 
 app.startProcesses = function() {
@@ -1048,7 +881,7 @@ var publicApi = {
 };
 
 module.exports = publicApi;
-},{"./activity.js":1,"./labels.js":3,"./sensors.js":4,"./skynet.js":5,"./topics.js":6}],3:[function(_dereq_,module,exports){
+},{"./activity.js":1,"./labels.js":3,"./push.js":4,"./sensors.js":5,"./skynet.js":6,"./topics.js":7}],3:[function(_dereq_,module,exports){
 var Q = Promise;
 
 var defer = function () {
@@ -1104,6 +937,183 @@ self.getLabel = function (lbl) {
 
 module.exports = self;
 },{}],4:[function(_dereq_,module,exports){
+var activity = _dereq_('./activity.js');
+
+module.exports = function() {
+    return new Promise(function(done, error) {
+        var started = false;
+        var push = window.PushNotification;
+
+        function isEnabled() {
+            return new Promise(function(resolve, reject) {
+                push.isPushEnabled(function(status) {
+                    console.log('Push Status: ' + JSON.stringify(status));
+                    if (status || status === 'OK') {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                });
+            });
+        }
+
+        function registerPushID(pushID) {
+            app.pushID = pushID;
+            window.localStorage.setItem('pushID', app.pushID);
+
+            return new Promise(function(resolve, reject) {
+                app.updateDeviceSetting({})
+                    .then(function() {
+                        activity.logActivity({
+                            type: 'push',
+                            html: 'Push ID Registered'
+                        });
+                        resolve();
+                    }, function() {
+                        var msg = 'Push ID Updated Failed';
+                        activity.logActivity({
+                            type: 'push',
+                            error: new Error(msg)
+                        });
+                        reject();
+                    });
+            });
+        }
+
+        function getPushID() {
+            return new Promise(function(resolve, reject) {
+                if (app.pushID) return resolve();
+                push.getPushID(function(pushID) {
+                    console.log('Push ID: ' + pushID);
+
+                    activity.logActivity({
+                        type: 'push',
+                        html: 'Push ID: ' + JSON.stringify(pushID)
+                    });
+
+                    if (pushID) {
+                        resolve(pushID);
+                    } else {
+                        reject();
+                    }
+                });
+            });
+        }
+
+        function enable() {
+            return new Promise(function(resolve, reject) {
+                push.enablePush(resolve);
+            });
+        }
+
+        function start() {
+            if (started) return console.log('Starting Push Notifications Logic');
+
+            if (!push) return console.log('No Push Object');
+
+            console.log('Starting Push Flow');
+
+            var a = push.notificationType.badge,
+                b = push.notificationType.sound,
+                c = push.notificationType.alert;
+
+            push.registerForNotificationTypes(a | b | c);
+
+            isEnabled()
+                .then(getPushID, function() {
+                    return enable()
+                        .then(getPushID, function() {
+                            activity.logActivity({
+                                type: 'push',
+                                error: 'Unable to get Push ID'
+                            });
+                        })
+                        .then(registerPushID, function() {
+                            activity.logActivity({
+                                type: 'push',
+                                error: 'Unable to register Push ID'
+                            });
+                        });
+                })
+                .finally(function() {
+                    started = true;
+                    done();
+                }, error);
+        }
+
+        function onRegistration(event) {
+            if (event.error) {
+                // Not Registered
+                var msg = 'Urbanairship Registration Error';
+
+                activity.logActivity({
+                    type: 'push',
+                    error: event.error
+                });
+
+                deferred.reject(msg);
+
+            } else {
+
+                activity.logActivity({
+                    type: 'push',
+                    html: 'Push ID Registered: ' + event.pushID
+                });
+
+                // Registered
+                app.pushID = event.pushID;
+                window.localStorage.setItem('pushID', app.pushID);
+
+                // Start Listen
+                start();
+            }
+        }
+
+        function handleIncomingPush(event) {
+            if (!event.message) {
+                console.log('Invalid Message');
+                return;
+            }
+            console.log('Incoming push: ' + event.message);
+
+            activity.logActivity({
+                type: 'push',
+                html: 'Received Push Notification: ' + event.message
+            });
+
+        }
+
+        function onDeviceReady() {
+            document.addEventListener('resume', function() {
+                console.log('Push: Device resume!');
+
+                push.resetBadge();
+                push.getIncoming(handleIncomingPush);
+
+                // Re-register for urbanairship events if they were removed in pause event
+                document.addEventListener('urbanairship.registration', onRegistration, false);
+                document.addEventListener('urbanairship.push', handleIncomingPush, false);
+            }, false)
+
+            document.addEventListener('pause', function() {
+                console.log('Push: Device pause!');
+                // Remove urbanairship events.  Important on android to not receive push in the background.
+                document.removeEventListener('urbanairship.registration', onRegistration, false);
+                document.removeEventListener('urbanairship.push', handleIncomingPush, false);
+            }, false);
+
+            document.addEventListener('urbanairship.registration', onRegistration, false);
+            document.addEventListener('urbanairship.push', handleIncomingPush, false);
+
+            start();
+        }
+
+        document.addEventListener('deviceready', onDeviceReady, false);
+
+    });
+
+}
+},{"./activity.js":1}],5:[function(_dereq_,module,exports){
 'use strict';
 
 var limit = 50;
@@ -1305,7 +1315,7 @@ module.exports = {
         };
     }
 };
-},{}],5:[function(_dereq_,module,exports){
+},{}],6:[function(_dereq_,module,exports){
 'use strict';
 var Q = Promise;
 
@@ -1512,7 +1522,7 @@ obj.getIPAddress = function(){
 };
 
 module.exports = obj;
-},{}],6:[function(_dereq_,module,exports){
+},{}],7:[function(_dereq_,module,exports){
 'use strict';
 
 var lib = {},
---
+++ www/public/skynet/push.js
@@ -0,0 +1,176 @@
+var activity = require('./activity.js');
+
+module.exports = function() {
+    return new Promise(function(done, error) {
+        var started = false;
+        var push = window.PushNotification;
+
+        function isEnabled() {
+            return new Promise(function(resolve, reject) {
+                push.isPushEnabled(function(status) {
+                    console.log('Push Status: ' + JSON.stringify(status));
+                    if (status || status === 'OK') {
+                        resolve();
+                    } else {
+                        reject();
+                    }
+                });
+            });
+        }
+
+        function registerPushID(pushID) {
+            app.pushID = pushID;
+            window.localStorage.setItem('pushID', app.pushID);
+
+            return new Promise(function(resolve, reject) {
+                app.updateDeviceSetting({})
+                    .then(function() {
+                        activity.logActivity({
+                            type: 'push',
+                            html: 'Push ID Registered'
+                        });
+                        resolve();
+                    }, function() {
+                        var msg = 'Push ID Updated Failed';
+                        activity.logActivity({
+                            type: 'push',
+                            error: new Error(msg)
+                        });
+                        reject();
+                    });
+            });
+        }
+
+        function getPushID() {
+            return new Promise(function(resolve, reject) {
+                if (app.pushID) return resolve();
+                push.getPushID(function(pushID) {
+                    console.log('Push ID: ' + pushID);
+
+                    activity.logActivity({
+                        type: 'push',
+                        html: 'Push ID: ' + JSON.stringify(pushID)
+                    });
+
+                    if (pushID) {
+                        resolve(pushID);
+                    } else {
+                        reject();
+                    }
+                });
+            });
+        }
+
+        function enable() {
+            return new Promise(function(resolve, reject) {
+                push.enablePush(resolve);
+            });
+        }
+
+        function start() {
+            if (started) return console.log('Starting Push Notifications Logic');
+
+            if (!push) return console.log('No Push Object');
+
+            console.log('Starting Push Flow');
+
+            var a = push.notificationType.badge,
+                b = push.notificationType.sound,
+                c = push.notificationType.alert;
+
+            push.registerForNotificationTypes(a | b | c);
+
+            isEnabled()
+                .then(getPushID, function() {
+                    return enable()
+                        .then(getPushID, function() {
+                            activity.logActivity({
+                                type: 'push',
+                                error: 'Unable to get Push ID'
+                            });
+                        })
+                        .then(registerPushID, function() {
+                            activity.logActivity({
+                                type: 'push',
+                                error: 'Unable to register Push ID'
+                            });
+                        });
+                })
+                .finally(function() {
+                    started = true;
+                    done();
+                }, error);
+        }
+
+        function onRegistration(event) {
+            if (event.error) {
+                // Not Registered
+                var msg = 'Urbanairship Registration Error';
+
+                activity.logActivity({
+                    type: 'push',
+                    error: event.error
+                });
+
+                deferred.reject(msg);
+
+            } else {
+
+                activity.logActivity({
+                    type: 'push',
+                    html: 'Push ID Registered: ' + event.pushID
+                });
+
+                // Registered
+                app.pushID = event.pushID;
+                window.localStorage.setItem('pushID', app.pushID);
+
+                // Start Listen
+                start();
+            }
+        }
+
+        function handleIncomingPush(event) {
+            if (!event.message) {
+                console.log('Invalid Message');
+                return;
+            }
+            console.log('Incoming push: ' + event.message);
+
+            activity.logActivity({
+                type: 'push',
+                html: 'Received Push Notification: ' + event.message
+            });
+
+        }
+
+        function onDeviceReady() {
+            document.addEventListener('resume', function() {
+                console.log('Push: Device resume!');
+
+                push.resetBadge();
+                push.getIncoming(handleIncomingPush);
+
+                // Re-register for urbanairship events if they were removed in pause event
+                document.addEventListener('urbanairship.registration', onRegistration, false);
+                document.addEventListener('urbanairship.push', handleIncomingPush, false);
+            }, false)
+
+            document.addEventListener('pause', function() {
+                console.log('Push: Device pause!');
+                // Remove urbanairship events.  Important on android to not receive push in the background.
+                document.removeEventListener('urbanairship.registration', onRegistration, false);
+                document.removeEventListener('urbanairship.push', handleIncomingPush, false);
+            }, false);
+
+            document.addEventListener('urbanairship.registration', onRegistration, false);
+            document.addEventListener('urbanairship.push', handleIncomingPush, false);
+
+            start();
+        }
+
+        document.addEventListener('deviceready', onDeviceReady, false);
+
+    });
+
+}
